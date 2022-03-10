02078592ab9198f3652316616592e166fa2515b7
Amir Farahani <Amirferyg@gmail.com>
2022-03-10 17:09:49 +0330

    Added server headers

---
+++ Src/Server/ServerAPI.hpp
@@ -0,0 +1,8 @@
+#ifndef FTP_SERVER_SERVERAPI_HPP
+#define FTP_SERVER_SERVERAPI_HPP
+class ServerAPI{
+public:
+    ServerAPI();
+    void Run();
+};
+#endif
---
+++ Src/Server/ServerCore.hpp
@@ -0,0 +1,4 @@
+#ifndef FTP_SERVER_SERVERCORE_HPP
+#define FTP_SERVER_SERVERCORE_HPP
+
+#endif
