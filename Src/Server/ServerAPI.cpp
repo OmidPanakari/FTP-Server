2a43ac7107c7ecd0cb253139218308961a951b53
Amir Farahani <Amirferyg@gmail.com>
2022-03-18 11:32:31 +0330

    Added CheckingFunctions

--- Src/Server/ServerAPI.cpp
+++ Src/Server/ServerAPI.cpp
@@ -1,5 +1,90 @@
 #include "ServerAPI.hpp"
+#include "Utility.hpp"
 
-std::string ServerAPI::HandleCommand(std::string command) {
+using namespace std;
 
+
+string ServerAPI::HandleCommand(string command) {
+    vector<string> commandVector = Split(command,' ');
+    if (commandVector.size() < 1) return "501";
+    if (commandVector[0] == USER){
+        commandVector.erase();
+        return CheckUsername(commandVector);
+    } else if (commandVector[0] == PASS){
+        commandVector.erase();
+        return Authenticate(commandVector);
+    } else if (commandVector[0] == PWD){
+        commandVector.erase();
+        return GetCurrentDirectory(commandVector);
+    } else if (commandVector[0] == MKD){
+        commandVector.erase();
+        return MakeDirectory(commandVector);
+    } else if(commandVector[0] == DELE){
+        commandVector.erase();
+        return DeleteFileOrDirectory(commandVector);
+    } else if(commandVector[0] == LS){
+        commandVector.erase();
+        return ShowList(commandVector);
+    } else if (commandVector[0] == CWD){
+        commandVector.erase();
+        return ChangeDirectory(commandVector);
+    } else if(commandVector[0] == RENAME){
+        commandVector.erase();
+        return RenameFile(commandVector);
+    } else if (commandVector[0] == RETR){
+        commandVector.erase();
+        return DownloadFile(commandVector);
+    } else if (commandVector[0] == HELP){
+        commandVector.erase();
+        return Help(commandVector);
+    } else if(commandVector[0] == QUIT){
+        commandVector.erase();
+        return Quit(commandVector);
+    } else return "501";
+}
+
+string ServerAPI::CheckUsername(vector <string> command) {
+    if (command.size() != 1) return "501";
+
+}
+
+std::string ServerAPI::Authenticate(std::vector <std::string> command) {
+    if (command.size() != 1) return "501";
+}
+
+std::string ServerAPI::GetCurrentDirectory(std::vector <std::string> command) {
+    if (command.size() != 0) return "501";
+}
+
+std::string ServerAPI::MakeDirectory(std::vector <std::string> command) {
+    if (command.size() != 1) return "501";
+}
+
+std::string ServerAPI::DeleteFileOrDirectory(std::vector <std::string> command) {
+    if (command.size() != 2) return "501";
+    if (command[0] != "-d" && command[0] != "-f") return "501";
+}
+
+std::string ServerAPI::ShowList(std::vector <std::string> command) {
+    if (command.size() != 0) return "501";
+}
+
+std::string ServerAPI::ChangeDirectory(std::vector <std::string> command) {
+    if (command.size() != 1) return "501";
+}
+
+std::string ServerAPI::RenameFile(std::vector <std::string> command) {
+    if (command.size() != 2) return "501";
+}
+
+std::string ServerAPI::DownloadFile(std::vector <std::string> command) {
+    if (command.size() != 1) return "501";
+}
+
+std::string ServerAPI::Help(std::vector <std::string> command) {
+    if (command.size() != 0) return "501";
+}
+
+std::string ServerAPI::Quit(std::vector <std::string> command) {
+    if (command.size() != 0) return "501";
 }
--- Src/Server/ServerAPI.hpp
+++ Src/Server/ServerAPI.hpp
@@ -1,8 +1,34 @@
 #include <string>
+#include <vector>
 
 #ifndef FTP_SERVER_SERVERAPI_HPP
 #define FTP_SERVER_SERVERAPI_HPP
+
+#define USER user
+#define PASS pass
+#define PWD pwd
+#define MKD mkd
+#define DELE dele
+#define LS ls
+#define CWD cwd
+#define RENAME rename
+#define RETR retr
+#define HELP help
+#define QUIT quit
+
 class ServerAPI{
+private:
+    std::string CheckUsername(std::vector<std::string>);
+    std::string Authenticate(std::vector<std::string>);
+    std::string MakeDirectory(std::vector<std::string>);
+    std::string DeleteFileOrDirectory(std::vector<std::string>);
+    std::string ChangeDirectory(std::vector<std::string>);
+    std::string RenameFile(std::vector<std::string>);
+    std::string DownloadFile(std::vector<std::string>);
+    std::string GetCurrentDirectory(std::vector<std::string>);
+    std::string ShowList(std::vector<std::string>);
+    std::string Help(std::vector<std::string>);
+    std::string Quit(std::vector<std::string>);
 public:
     ServerAPI();
     void Run();
